b25c82ce5c415575bd668348ce7bd3e86b567d0d
vpavan <vpavan8747@gmail.com>
2024-02-28 21:49:37 +0530

    1.5 Error handling

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println("****** String To Integer Convertion *****")
+	stringToNum()
+}
+
+func stringToNum() {
+	str := "hi"
+	num, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(num)
 }
